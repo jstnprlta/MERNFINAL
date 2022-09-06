811249d8aa6e9a6312a37323bcbe8a1743092da1
EduardoDelgado15 <Eddydelgado15@yahoo.com>
2022-09-06 04:31:25 -0700

    origin main

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,36 @@
-const Home = () => {
+import { useEffect } from "react"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
 
-    return (
-      <div className="Home">
-        <h2>Home</h2>
+// components
+import WorkoutDetails from "../components/WorkoutDetails"
+import WorkoutForm from "../components/WorkoutForm"
+
+const Home = () => {
+  const { workouts, dispatch } = useWorkoutsContext()
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('./api/workouts')
+      const json = await response.json()
+
+      if (response.ok) {
+        dispatch({type: 'SET_WORKOUTS', payload: json})
+      }
+    }
+
+    fetchWorkouts()
+  }, [dispatch])
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts && workouts.map(workout => (
+          <WorkoutDetails workout={workout} key={workout._id} />
+        ))}
       </div>
-    )
-  }
-  
-  export default Home
+      <WorkoutForm />
+    </div>
+  )
+}
+
+export default Home
